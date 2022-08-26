552073368123e0c5fb5dd3b0a4759802c823947b
LUISFFSILVA <luis.silva1102@gmail.com>
2022-08-25 23:23:05 -0300

    Aula04

--- ProjetoEventos/nodeEventos/controllers/eventos.js
+++ ProjetoEventos/nodeEventos/controllers/eventos.js
@@ -1,36 +1,58 @@
-module.exports = function(app){
+module.exports = function (app) {
+
+    var Evento = app.models.eventos
+
     var EventosController = {
-        menu: function(request,response){
+        menu: function (request, response) {
 
             var usuario = request.session.usuario,
-                params = {usuario: usuario}
+                params = { usuario: usuario }
             response.render('eventos/menu', params)
         },
 
-        cadastroUsuario: function(request,response){
+        cadastroUsuario: function (request, response) {
             var usuario = request.session.usuario,
-                params = {usuario: usuario}
+                params = { usuario: usuario }
             response.render('eventos/cadUsuario', params)
         },
 
-        cadastroEvento: function(request,response){
+        cadastroEvento: function (request, response) {
             var usuario = request.session.usuario,
-                params = {usuario: usuario}
+                params = { usuario: usuario }
             response.render('eventos/cadEvento', params)
         },
 
-        listaEventos: function(request,response){
-            var usuario = request.session.usuario,
-                params = {usuario: usuario}
-                response.render('eventos/listaEventos',params)
+        listaEventos: function (request, response) {
+            Evento.find(function (erro, eventos) {
+                if (erro) {
+                    response.render('/menu')
+                }
+                else {
+                    var usuario = request.session.usuario,
+                        params = { usuario: usuario, eventos: eventos }
+                    response.render('eventos/listaEventos', params)
+                }
+            })
         },
 
         //cadastro de eventos
         novoEvento: function (request, response) {
-        //código a ser implementado
-        response.redirect('/menu');
+            var evento = request.body.evento;
+            if (evento.descricao.trim().length == 0 || evento.data == 'undefined'
+                || evento.preco.trim().length == 0) {
+                response.redirect('/cadEvento');
+            }
+            else {
+                Evento.create(evento, function (erro, evento) {
+                    if (erro) {
+                        response.redirect('/cadEvento');
+                    }
+                    else {
+                        response.redirect('/menu');
+                    }
+                });
+            }
         }
-
-    }
+    };
     return EventosController
 }
--- ProjetoEventos/nodeEventos/controllers/home.js
+++ ProjetoEventos/nodeEventos/controllers/home.js
@@ -1,9 +1,32 @@
 module.exports = function (app){
+
+    var mongoose = require('mongoose');
+    var Usuario = mongoose.model('usuarios')
+
+
     var HomeController = {
         index: function (req,res)   {
         res.render('home/index')
     },
 
+    login: function(req,res){
+        var nome = request.body.usuario.nome
+        var senha = request.body.usuario.senha
+
+        var query = {'nome': nome, 'senha':senha}
+
+        Usuario.findOne(query).select('nome senha')
+            .exec(function(erro,usuario){
+                if(erro){
+                    res.redirect('/')
+                }
+                else {
+                    req.session.usuario = usuario
+                    res.redirect('/menu')
+                }
+            })
+    },
+/*
     login: function(req,res){
 
         var nome = req.body.usuario.nome
@@ -19,7 +42,7 @@ module.exports = function (app){
             res.redirect('/')
         }
     },
-
+*/
     logout: function(request,response){
         request.session.destroy()
         response.redirect('/')
@@ -31,9 +54,22 @@ module.exports = function (app){
         var senha = request.body.usuario.senha
         var confirma = request.body.usuario.conmfirma
 
-        response.redirect('/menu')
-    }
-}
+        if ((senha != confirma) || nome.trim().length == 0) {
+            response.redirect('/');
+            }
+            else {
+            var usuario = request.body.usuario;
+            Usuario.create(usuario, function (erro, usuario) {
+            if (erro) {
+            response.redirect('/');
+            }
+            else {
+            response.redirect('/menu');
+            }
+            });
+            }
+            }
+            };
     return HomeController
 }
 
